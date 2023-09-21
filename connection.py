c221eacae91f28968a77eaf33c65582208d4bdf2
Kinceli Antal <antal.kinceli@gmail.com>
2023-09-21 18:32:17 +0200

    Add csv data storage connection

---
+++ connection.py
@@ -0,0 +1,14 @@
+import csv
+
+
+def write_data(source, data):
+    with open(source, 'a') as file:
+        csv.DictWriter(file).writerows(data)
+
+
+def load_data(source):
+    try:
+        with open(source, newline='') as file:
+            return csv.DictReader(file)
+    except FileNotFoundError:
+        print('CSV file not found.')
